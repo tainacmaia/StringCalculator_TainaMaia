e0b94d2ad199b62f64e0a052dda8526e0a6b3fb2
Tainá Maia <taina.cmaia@hotmail.com>
2022-09-28 21:51:53 -0300

    Add project files.

---
+++ StringCalculator.Domain/Models/StringCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Domain.Models
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var parts = numbers.Split(',');
+
+            var exceedsCount = parts.Length > 2;
+            var consecutiveCommas = parts.Any(x => x == "");
+            var nonNumbers = parts.Any(x => !int.TryParse(x, out _));
+            
+            if (exceedsCount || consecutiveCommas || nonNumbers)
+                throw new ArgumentException(nameof(numbers));
+            
+            var sum = parts.Sum(x => Convert.ToInt32(x));
+
+            return sum;
+        }
+    }
+}
---
+++ StringCalculator.UnitTests/StringCalculatorUnitTest.cs
@@ -0,0 +1,66 @@
+using FluentAssertions;
+
+namespace StringCalculator.UnitTests
+{
+    public class StringCalculatorUnitTest
+    {
+        [Fact]
+        public void StringCalculator_EmptyString_ResultShouldBeZero()
+        {
+            var sut = new Domain.Models.StringCalculator();
+
+            int result = sut.Add("");
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void StringCalculator_Null_ResultShouldBeZero()
+        {
+            var sut = new Domain.Models.StringCalculator();
+
+            int result = sut.Add(null);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void StringCalculator_ThreeNumbersSeparatedByCommas_ShouldThrowException()
+        {
+            var sut = new Domain.Models.StringCalculator();
+
+            Action addDecimal = () => sut.Add("1,2,3");
+
+            addDecimal.Should().Throw<ArgumentException>().WithMessage("*numbers*");
+        }
+        [Fact]
+        public void StringCalculator_WhenConsecutiveCommas_ShouldThrowException()
+        {
+            var sut = new Domain.Models.StringCalculator();
+
+            Action addDecimal = () => sut.Add("1,,3");
+
+            addDecimal.Should().Throw<ArgumentException>().WithMessage("*numbers*");
+        }
+
+        [Fact]
+        public void StringCalculator_WhenContainsNonNumbers_ShouldThrowException()
+        {
+            var sut = new Domain.Models.StringCalculator();
+
+            Action addDecimal = () => sut.Add("1,a");
+
+            addDecimal.Should().Throw<ArgumentException>().WithMessage("*numbers*");
+        }
+
+        [Fact]
+        public void StringCalculator_WhenValidInput_ShouldReturnSum()
+        {
+            var sut = new Domain.Models.StringCalculator();
+
+            var result = sut.Add("1,3");
+
+            result.Should().Be(4);
+        }
+    }
+}
